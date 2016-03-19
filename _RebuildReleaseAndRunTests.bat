f0a5dd75b740c015434fd717882b175cf65d4d1f
Friedrich von Never <friedrich@fornever.me>
2016-03-19 21:18:51 +0600

    Rebuild: use release build

--- _RebuildReleaseAndRunTests.bat
+++ _RebuildReleaseAndRunTests.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /t:rebuild /property:Configuration=Debug PascalABCNET.sln
+%windir%\microsoft.net\framework\v4.0.30319\msbuild /t:rebuild /property:Configuration=Release PascalABCNET.sln
 @IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 cd ReleaseGenerators
